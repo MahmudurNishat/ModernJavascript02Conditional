c7def8935e41bc9db7800e98ef55dc71dd82131a
MahmudurNishat <rajbiroy23@gmail.com>
2024-03-23 17:48:25 +0600

    add more compare in readme file and compare.js file

--- Compare.js
+++ Compare.js
@@ -1,12 +1,41 @@
+// Comparison operatior
+// *   Larger >
+// *   Less <
+// *   Equal ==
+// *   Greater than or equal >=
+// *   Less than  or equal <=
+// *   Not equal !=
+// *   And &&
+// *   Or ||
 
-/** Comparison operatior 
- * * 1. Larger >
- * * 2. Less <
- * * 3. Equal ==
- * * 4. Greater than or equal >=
- * * 5. Less than  or equal <=
- * * 6. Not equal !=
- * * 7. And &&
- * * 8. Or ||
- * 
- */
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(15 >= 10);
+console.log(15 <= 10);
+console.log(5 <= 10);
+console.log(100 <= 10);
+console.log(10 == 10.1);
+
+
+// variable comparison  
+var applePrice = 250;
+var peyeraPrice = 40;
+
+console.log(applePrice>peyeraPrice)
+console.log(applePrice<peyeraPrice)
+console.log(applePrice<=peyeraPrice)
+console.log(applePrice>=peyeraPrice)
+
+// double equal == and threple qual ===
+
+console.log(10 == "10") // output : true, it always check both values not type .se dektese value same tai true , aeta type check kore na
+console.log(10 === "10") // output : false, it always check both values and type  se dektese value same then type check korbe ekta number and arekta string . tai false
+
+
+// not equal != and double not equal  !==
+
+console.log(10 != "10") // output : false,  aekane just se value check korbe , se dekce 10 , 10 er soman , kintu tomi bolco soman na , tai output : false
+
+console.log(10 !== "10"); // output : true , karon se aekane value first e check korbe ,se dektece value thik ase , then check korbe type , se dekce ekta arrekta string , tai jokon bola hocce 10 string 10 er soman na , tokon se output dekabe : true
--- readme.md
+++ readme.md
@@ -11,7 +11,38 @@
  *   And &&
  *   Or ||
 
+console.log(5 > 10); <br>
+console.log(5 < 10);<br>
+console.log(5 == 10);<br>
+console.log(10 == 10);<br>
+console.log(15 >= 10);<br>
+console.log(15 <= 10);<br>
+console.log(5 <= 10);<br>
+console.log(100 <= 10);<br>
+console.log(10 == 10.1);<br>
 
 
+// variable comparison  <br>
+var applePrice = 250;<br>
+var peyeraPrice = 40;<br>
 
-     Know the Js comparison operator  <End>
+console.log(applePrice>peyeraPrice)<br>
+console.log(applePrice<peyeraPrice)<br>
+console.log(applePrice<=peyeraPrice)<br>
+console.log(applePrice>=peyeraPrice)<br>
+
+// double equal == and threple qual ===<br>
+
+console.log(10 == "10") // output : true, it always check both values not type .se dektese value same tai<br> true , aeta type check kore na<br>
+console.log(10 === "10") // output : false, it always check both values and type  se dektese value same<br> then type check korbe ekta number and arekta string . tai false<br>
+
+
+// not equal != and double not equal  !==<br>
+
+console.log(10 != "10") // output : false,  aekane just se value check korbe , se dekce 10 , 10 er soman ,<br> kintu tomi bolco soman na , tai output : false<br>
+
+console.log(10 !== "10"); // output : true , karon se aekane value first e check korbe ,se dektece value<br> thik ase , then check korbe type , se dekce ekta arrekta string , tai jokon bola hocce 10 string <br>10 er soman na , tokon se output dekabe : true<br>
+
+        Know the Js comparison operator  <End>
+=================================================
+
